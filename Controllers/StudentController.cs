1ddce5e3bcb15b9d1f227c1b5eec790555d45525
boil0035 <wboileau76@gmail.com>
2021-06-09 23:25:21 -0400

    Initial Commit MVC Demo App

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,36 @@
+﻿using MVCDemoApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemoApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ListStudent()
+        {
+            List<StudentModel> student = new List<StudentModel>();
+            student.Add(new StudentModel { FirstName = "John", LastName = "Doe", Major="Business", Course="Distributed Info" });
+            student.Add(new StudentModel { FirstName = "Jane", LastName = "Doe", Major = "Accounting", Course = "Financial Acct 1" });
+            student.Add(new StudentModel { FirstName = "Michael", LastName = "Jay", Major = "Engineer", Course = "Mechanical 1" });
+            // Get the list to the view
+            return View(student);
+        }
+        [HttpGet]
+        public ActionResult Edit( int id)
+        {
+            StudentModel student = new StudentModel();
+            
+
+            return View("Edit",student);
+        
+        }
+    }
+}
---
+++ Models/StudentModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+// Test Source : https://www.youtube.com/watch?v=phyV-OQNeRM
+namespace MVCDemoApp.Models
+{
+    public class StudentModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Major { get; set; }
+        public string Course { get; set; }
+        public bool IsEnroll { get; set; } = true;
+    }
+
+    
+
+}
